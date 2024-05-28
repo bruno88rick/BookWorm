8e186e7ea844cb3e990c914e0c8f3047e671d430
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-28 17:20:17 -0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Bruno Oliveira on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
