4b6bcd84c01d707562772272cb5a6a8459b712ef
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-29 13:42:53 -0300

    DetailView Completed

---
+++ BookWorm/DetailView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailView.swift
+//  BookWorm
+//
+//  Created by Bruno Oliveira on 29/05/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.largeTitle)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+            
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Livro de Teste", author: "Autor de Teste", genre: "Horror", review: "Este livro  é muito bom e eu gostei bastande dele.", rating: 3)
+        
+        return DetailView(book: example)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
