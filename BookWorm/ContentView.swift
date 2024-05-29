e281744ae4ed90b71e5e8756a46d6c07d3c1da94
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-29 15:51:15 -0300

    Sorting content from SwiftData on Content View

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    //@Query var books: [Book]
+    //using sort to sorting data. We thave some ways to sort, even using SorDescriptor to sort by one or more fields
+    //@Query(sort: \Book.title) var books: [Book]
+    //@Query(sort: \Book.rating, order: .reverse) var books: [Book]
+    //@Query(sort: [SortDescriptor(\Book.title)]) var books: [Book]
+    //@Query(sort: [SortDescriptor(\Book.title, order: .reverse)]) var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author),
+        SortDescriptor(\Book.rating, order: .reverse)
+    ]) var books: [Book]
     
     @State private var showingAddScreen = false
     
