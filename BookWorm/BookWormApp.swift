2bcf42c68ef4af07c09c3425ff8497064ccfe4de
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-29 09:10:32 -0300

    Initial Commit

--- BookWorm/BookWormApp.swift
+++ BookWorm/BookWormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Bruno Oliveira on 28/05/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookWormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -5,17 +5,30 @@
 //  Created by Bruno Oliveira on 28/05/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count) books")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen, content: {
+                    AddBookView()
+                })
         }
-        .padding()
     }
 }
 
